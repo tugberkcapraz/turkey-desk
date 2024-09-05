1a3eb82c5b219521eab390f82ab397bd71174356
tugberkcapraz <tugberkcapraz@gmail.com>
2024-09-05 14:13:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Globe from "react-globe.gl"; // Import Globe as the default export
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Globe />
   );
 }
 
